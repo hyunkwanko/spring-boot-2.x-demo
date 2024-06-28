6bb657d7e2ccbd3d5dc552ff9bfdbd729e776aea
khk <cjdshrwkd11@gmail.com>
2024-06-28 13:51:32 +0900

    intial

---
+++ README.md
@@ -0,0 +1,7 @@
+## Spring Boot 2.x Demo
+### 2024.06.28 기준
+* Spring Boot 2.7.16
+* Java 11
+* Gradle 7.1.1
+* Sonarqube 5.0.0.4638
+* Jacoco 0.8.11
---
+++ src/test/java/sample/cafekiosk/CafekioskApplicationTests.java
@@ -0,0 +1,14 @@
+package sample.cafekiosk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import sample.cafekiosk.spring.CafekioskApplication;
+
+@SpringBootTest(classes = CafekioskApplication.class)
+class CafekioskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
